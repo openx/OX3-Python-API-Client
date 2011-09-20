13795fb1b393c7678e1bef11441b8d05992c00c6
Tony Edwards <tnydwrds@gmail.com>
2011-09-20 18:33:20 -0400

    Add OX3APIClient.request method to support generic HTTP requests.
    Added urllib2 dependency.
    Added HTTP_METHOD_OVERRIDES to support PUT and DELETE in addition to
    GET and POST HTTP requests.

--- ox3apiclient.py
+++ ox3apiclient.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import oauth2 as oauth
+import urllib2
 
 REQUEST_TOKEN_URL = 'https://sso.openx.com/api/index/initiate'
 ACCESS_TOKEN_URL = 'https://sso.openx.com/api/index/token'
 AUTHORIZATION_URL = 'https://sso.openx.com/login/process'
 API_PATH = '/ox/3.0/a'
+HTTP_METHOD_OVERRIDES = ['DELETE', 'PUT']
 
 class OX3APIClient(object):
     
@@ -44,4 +46,21 @@ class OX3APIClient(object):
         self._consumer = oauth.Consumer(self.consumer_key, self.consumer_secret)
         self._token = oauth.Token('', '')
     
+    def request(self, url, method='GET', headers={}, data=None, sign=False):
+        """Helper method to make a HTTP request."""
+        
+        # Since we are using a urllib2.Request object we need to assign a value
+        # other than None to "data" in order to make the request a POST request,
+        # even if there is no data to post.
+        if method == 'POST':
+            data = data if data else ''
+        
+        req = urllib2.Request(url, headers=headers, data=data)
+        
+        # We need to set the request's get_method function to return a HTTP
+        # method for any values other than GET or POST.
+        if method in HTTP_METHOD_OVERRIDES:
+            req.get_method = lambda: method
+        
+        return urllib2.urlopen(req)
 
