520ea3dafdcb07cff47b5db4cedee4100c6a9362
Don Rowe <rowedonalde@gmail.com>
2013-03-11 16:15:28 -0700

    Revert "Removed references to the oAuth realm since"
    This reverts commit 2e36ea253599630d359c309c8ae6e5572338c324.
    
    Conflicts:
    	ox3apiclient/__init__.py

--- ox3apiclient/__init__.py
+++ ox3apiclient/__init__.py
@@ -43,7 +43,7 @@ HTTP_METHOD_OVERRIDES = ['DELETE', 'PUT']
 
 class Client(object):
 
-    def __init__(self, domain, consumer_key, consumer_secret,
+    def __init__(self, domain, realm, consumer_key, consumer_secret,
                     callback_url='oob',
                     scheme='http',
                     request_token_url=REQUEST_TOKEN_URL,
@@ -57,6 +57,8 @@ class Client(object):
         """
 
         domain -- Your UI domain. The API is accessed off this domain.
+        realm -- Your sso realm. While not necessary for all OAuth
+            implementations, it is a requirement for OpenX Enterprise
         consumer_key -- Your consumer key.
         consumer_secret -- Your consumer secret.
         callback_url -- Callback URL to redirect to on successful authorization.
@@ -69,6 +71,7 @@ class Client(object):
         """
         
         self.domain = domain
+        self.realm = realm
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.callback_url = callback_url
@@ -130,6 +133,7 @@ class Client(object):
 
         # Update our original requests headers to include the OAuth Authorization
         # header and return it.
+        req.headers.update(oauth_req.to_header(realm=self.realm))
         return \
             urllib2.Request(req.get_full_url(), headers=req.headers, data=data)
 
@@ -366,6 +370,7 @@ def client_from_file(file_path='.ox3rc', env=None):
     # Required parameters for a ox3apiclient.Client instance.
     required_params = [
         'domain',
+        'realm',
         'consumer_key',
         'consumer_secret']
 
@@ -381,6 +386,7 @@ def client_from_file(file_path='.ox3rc', env=None):
 
     client = Client(
         domain=client_params['domain'],
+        realm=client_params['realm'],
         consumer_key=client_params['consumer_key'],
         consumer_secret=client_params['consumer_secret'])
 
