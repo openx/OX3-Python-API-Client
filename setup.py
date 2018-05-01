a7b66356e33ad568178a869475b281a4e8daddc5
Derek Kulinski <derek.kulinski@openx.com>
2018-05-01 16:04:26 -0700

    Packaging improvements.
    Improvements arount sdist and bdist_wheel generation.
    This change also allows to run tests by issuing
    `./setup.py test`

--- setup.py
+++ setup.py
@@ -2,22 +2,31 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 from os.path import dirname, join
+import os
 import re
+
+def read_file(name):
+    filename = join(dirname(__file__), name)
+    with open(filename) as fp:
+        return fp.read()
 
 file_path = join(dirname(__file__), 'ox3apiclient', '__init__.py')
 version = re.search("__version__\s*=\s*['\"](.+)['\"]",
                     open(file_path, 'r').read()).groups()[0]
 
 setup(name='ox3apiclient',
-      version=version,
-      author='OpenX API Team',
-      author_email='api@openx.com',
-      url='https://github.com/openx/OX3-Python-API-Client',
-      description='Client to connect to the OpenX Enterprise API.',
-      long_description='',
-      packages=find_packages(),
-      install_requires=['six','requests_oauthlib'],
-      classifiers=[
+    version=version,
+    author='OpenX API Team',
+    author_email='api@openx.com',
+    url='https://github.com/openx/OX3-Python-API-Client',
+    license='BSD',
+    platforms=['POSIX'],
+    description='Client to connect to the OpenX Enterprise API.',
+    long_description=read_file('README.md'),
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=['six','requests_oauthlib'],
+    classifiers=[
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -32,4 +41,9 @@ setup(name='ox3apiclient',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock; python_version < "3.3"'
+    ]
 )
